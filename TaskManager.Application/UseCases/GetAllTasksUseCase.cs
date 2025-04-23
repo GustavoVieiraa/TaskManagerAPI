8ad8be42f99aaf1d9178e77f6ab5569a0e8213b4
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-23 06:06:20 -0300

    feat/test: adicionado GetAllTasksUseCase com testes unitários para cenários com e sem tarefas

---
+++ TaskManager.Application/UseCases/GetAllTasksUseCase.cs
@@ -0,0 +1,20 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Application.UseCases
+{
+    public class GetAllTasksUseCase
+    {
+        private readonly ITaskRepository _repository;
+
+        public GetAllTasksUseCase(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<TaskItem> Execute()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
---
+++ TaskManager.UnitTests/Application/UseCases/GetAllTasksUseCaseTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using TaskManager.Application.UseCases;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.UnitTests.Application.UseCases
+{
+    public class GetAllTasksUseCaseTests
+    {
+        [Fact]
+        public void Execute_ShouldReturnAllTasks_WhenTasksExist()
+        {
+            var tasks = new List<TaskItem>
+            {
+                new TaskItem("Task 1", "Desc 1", DateTime.Today.AddDays(1)),
+                new TaskItem("Task 2", "Desc 2", DateTime.Today.AddDays(2)),
+            };
+
+            var repo = new Mock<ITaskRepository>();
+            repo.Setup(r => r.GetAll()).Returns(tasks);
+
+            var useCase = new GetAllTasksUseCase(repo.Object);
+
+            var result = useCase.Execute();
+
+            result.Should().HaveCount(2);
+            result.Should().BeEquivalentTo(tasks);
+
+        }
+
+        [Fact]
+        public void Execute_ShouldReturnEmptyList_WhenNoTasksExist()
+        {
+            var repo = new Mock<ITaskRepository>();
+            repo.Setup(r => r.GetAll()).Returns(new List<TaskItem>());
+
+            var useCase = new GetAllTasksUseCase(repo.Object);
+
+            var result = useCase.Execute();
+
+            result.Should().BeEmpty();
+        }
+    }
+}
