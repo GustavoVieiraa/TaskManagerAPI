847b9d2a08c284a5a26b929168a07bd55540a0b9
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-23 05:52:44 -0300

    feat/test: adicionado GetTaskByIdUseCase com testes unitários cobrindo cenários de sucesso e falha.

---
+++ TaskManager.Application/UseCases/GetTaskByIdUseCase.cs
@@ -0,0 +1,20 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Application.UseCases
+{
+    public class GetTaskByIdUseCase
+    {
+        private readonly ITaskRepository _repository;
+
+        public GetTaskByIdUseCase(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public TaskItem? Execute(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
---
+++ TaskManager.UnitTests/Application/UseCases/GetTaskByIdUseCaseTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Moq;
+using TaskManager.Application.UseCases;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.UnitTests.Application.UseCases
+{
+    public class GetTaskByIdUseCaseTests
+    {
+        [Fact]
+        public void Execute_ShouldReturnTask_WhenTaskExists()
+        {
+            var repo = new Mock<ITaskRepository>();
+            var task = new TaskItem("um titulo válido", "Clean Architecture", DateTime.Today.AddDays(3));
+
+            repo.Setup(r => r.GetById(task.Id)).Returns(task);
+
+            var useCase = new GetTaskByIdUseCase(repo.Object);
+
+            var result = useCase.Execute(task.Id);
+
+            result.Should().NotBeNull();
+            result.Should().Be(task);
+        }
+
+        [Fact]
+        public void Execute_ShouldReturnNull_WhenTaskDoesNotExist()
+        {
+            var repo = new Mock<ITaskRepository>();
+            var randomId = Guid.NewGuid();
+            repo.Setup(r => r.GetById(randomId)).Returns((TaskItem?)null);
+
+            var useCase = new GetTaskByIdUseCase(repo.Object);
+
+            var result = useCase.Execute(randomId);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Execute_ShouldCallRepositoryWithCorrectId()
+        {
+            var repo = new Mock<ITaskRepository>();
+            var id = Guid.NewGuid();
+            repo.Setup(r => r.GetById(id)).Returns((TaskItem?)null);
+
+            var useCase = new GetTaskByIdUseCase(repo.Object);
+
+            useCase.Execute(id);
+
+            repo.Verify(r => r.GetById(id), Times.Once);
+        }
+
+    }
+}
