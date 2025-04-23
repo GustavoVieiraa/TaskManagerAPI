5012c187eca0fb85d6c27d920f03fa7aeb40acb6
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-23 20:21:34 -0300

    feat/test: Adicionado DeleteTaskUseCase e seus testes unitários para deletar uma tarefa.

---
+++ TaskManager.Application/UseCases/DeleteTaskUseCase.cs
@@ -0,0 +1,28 @@
+﻿
+
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Application.UseCases
+{
+    public class DeleteTaskUseCase
+    {
+        private readonly ITaskRepository _repository;
+
+        public DeleteTaskUseCase(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool Execute(Guid id)
+        {
+            var task = _repository.GetById(id);
+            if (task is null)
+            {
+                return false;
+            }
+
+            _repository.Delete(task.Id);
+            return true;
+        }
+    }
+}
---
+++ TaskManager.UnitTests/Application/UseCases/DeleteTaskUseCaseTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Threading.Tasks;
+using TaskManager.Application.UseCases;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.UnitTests.Application.UseCases
+{
+    public class DeleteTaskUseCaseTests
+    {
+        [Fact]
+        public void Execute_ShouldDeleteTask_WhenTaskExists()
+        {
+            var task = new TaskItem("título Task", "Descrição válida", DateTime.Today.AddDays(1));
+            var repo = new Mock<ITaskRepository>();
+
+            repo.Setup(r => r.GetById(task.Id)).Returns(task);
+            var useCase = new DeleteTaskUseCase(repo.Object);
+
+            var result = useCase.Execute(task.Id);
+
+            result.Should().BeTrue();
+            repo.Verify(r => r.Delete(task.Id), Times.Once);
+
+        }
+
+        [Fact]
+        public void Execute_ShouldReturnFalse_WhenTaskDoesNotExist()
+        {
+            var repo = new Mock<ITaskRepository>();
+            repo.Setup(r => r.GetById(It.IsAny<Guid>())).Returns((TaskItem?)null);
+            var useCase = new DeleteTaskUseCase(repo.Object);
+
+            var result = useCase.Execute(Guid.NewGuid());
+
+            result.Should().BeFalse();
+        }
+    }
+}
