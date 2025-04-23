8abb32113b73fde4d1c787748a24676bcc08560c
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-23 06:25:39 -0300

    feat/test: Adicionado CompleteTaskUseCase e seus testes unitários para marcar tarefa como concluída.

---
+++ TaskManager.Application/UseCases/CompleteTaskUseCase.cs
@@ -0,0 +1,28 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Application.UseCases
+{
+    public class CompleteTaskUseCase
+    {
+        private readonly ITaskRepository _repository;
+
+        public CompleteTaskUseCase(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool Execute(Guid id)
+        {
+            var task = _repository.GetById(id);
+            if (task is null)
+            {
+                return false;
+            }
+
+            task.MarkAsCompleted();
+            _repository.Update(task);
+            return true;
+        }
+    }
+}
---
+++ TaskManager.UnitTests/Application/UseCases/CompleteTaskUseCaseTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using TaskManager.Application.UseCases;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.UnitTests.Application.UseCases
+{
+    public class CompleteTaskUseCaseTests
+    {
+        [Fact]
+        public void Execute_ShouldMarkTaskAsCompleted_WhenTaskExists()
+        {
+            var task = new TaskItem("Task Title", "Descrição", DateTime.Today.AddDays(1));
+            var repo = new Mock<ITaskRepository>();
+
+            repo.Setup(r => r.GetById(task.Id)).Returns(task);
+            var useCase = new CompleteTaskUseCase(repo.Object);
+
+            var result = useCase.Execute(task.Id);
+
+            result.Should().BeTrue();
+            task.IsCompleted.Should().BeTrue();
+
+            repo.Verify(r => r.Update(It.Is<TaskItem>(t => t.Id == task.Id && t.IsCompleted == true)), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_ShouldReturnFalse_WhenTaskDoesExist()
+        {
+            var repo = new Mock<ITaskRepository>();
+            repo.Setup(r => r.GetById(It.IsAny<Guid>())).Returns((TaskItem?)null);
+            var useCase = new CompleteTaskUseCase(repo.Object);
+            
+            var result = useCase.Execute(Guid.NewGuid());
+            
+            result.Should().BeFalse();
+
+            repo.Verify(r => r.Update(It.IsAny<TaskItem>()), Times.Never);
+        }
+    }
+}
